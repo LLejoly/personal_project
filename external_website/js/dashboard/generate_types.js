8e1ee1513ceb7f27f3951adc94c9fbe5b47831b8
Loic Lejoly <loiclejoly@gmail.com>
2018-04-10 21:17:10 +0200

    Dashboard improvements + ideas in the report

--- external_website/js/dashboard/generate_types.js
+++ external_website/js/dashboard/generate_types.js
@@ -1,10 +1,33 @@
+var beautifulHeader = {
+    "box_num": "box number",
+    "type_id": "type identification",
+    "type_name_en": "name",
+    "type_name_fr": "nom",
+    "freezer_id": "freezer identification",
+    "freezer_name": "freezer name",
+    "number_boxes": "number of boxes",
+    "date_formatted_in": "date in",
+    "date_formatted_out": "date out",
+    "product_name": "name",
+    "text_descr": "product description",
+    "period": "period (months)",
+    "quantity": "quantity (in terms of person)",
+    "prod_num": "product identification in the freezer",
+    "prod_id": "product identification in the DB"
+
+}
+
+$.myjQuery = function(id) {
+    $(id).tablesorter();
+ };
+
 function generateTableFromJson(content) {
-    var myBooks = content['content'];
+    var contentData = content['content'];
     // EXTRACT VALUE FOR HTML HEADER. 
     // ('Book ID', 'Book Name', 'Category' and 'Price')
     var col = [];
-    for (var i = 0; i < myBooks.length; i++) {
-        for (var key in myBooks[i]) {
+    for (var i = 0; i < contentData.length; i++) {
+        for (var key in contentData[i]) {
             if (col.indexOf(key) === -1) {
                 col.push(key);
             }
@@ -13,6 +36,8 @@ function generateTableFromJson(content) {
 
     // CREATE DYNAMIC TABLE.
     var table = document.createElement("table");
+    table.setAttribute("id", content['elementId'] + "_table");
+    table.setAttribute("class","tablesorter-bootstrap table-responsive table-hover table-responsive-md");
 
     // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
     var header = table.createTHead();
@@ -20,19 +45,23 @@ function generateTableFromJson(content) {
 
     for (var i = 0; i < col.length; i++) {
         var th = document.createElement("th");      // TABLE HEADER.
-        th.innerHTML = col[i];
+        th.setAttribute("class", "text-center")
+        if (beautifulHeader.hasOwnProperty(col[i]))
+            th.innerHTML = beautifulHeader[col[i]];
+        else
+            th.innerHTML = col[i];
         tr.appendChild(th);
     }
 
     var body = table.createTBody();
     // ADD JSON DATA TO THE TABLE AS ROWS.
-    for (var i = 0; i < myBooks.length; i++) {
+    for (var i = 0; i < contentData.length; i++) {
 
         tr = body.insertRow(-1);
 
         for (var j = 0; j < col.length; j++) {
             var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = myBooks[i][col[j]];
+            tabCell.innerHTML = contentData[i][col[j]];
         }
     }
 
@@ -40,4 +69,7 @@ function generateTableFromJson(content) {
     var divContainer = document.getElementById(content['elementId']);
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
+
+    //Call jquery function
+    $.myjQuery("#" + content['elementId'] + "_table"); 
 }
