24ed637428d72f9682b2efad4e3de12353e93c65
Loic Lejoly <loiclejoly@gmail.com>
2018-05-18 13:11:13 +0200

    change dir name + report introduction

--- flask_run.sh
+++ flask_run.sh
@@ -3,6 +3,6 @@ if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters"
     exit
 fi
-export FLASK_APP=./REST_api/server.py
+export FLASK_APP=./rest_api/server.py
 export DB_NAME=$1
 flask run
